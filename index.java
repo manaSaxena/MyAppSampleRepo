2a5b25d8406f79527b828f0750ccab79d9994db8
Manas Saxena <saxena.manas14@gmail.com>
2019-09-04 13:23:10 +0530

    Login file added

--- index.java
+++ index.java
@@ -2,5 +2,6 @@ class abc {
 	public static void main(String ar[])
 	{
 		System.out.println("Hello world");
+		System.out.println("Hello world.. index");
 	}
 }
---
+++ login.java
@@ -0,0 +1,6 @@
+Class XYZ{
+	public static void main (String ar [])
+	{
+		system.out.println("login file")
+	}
+}
